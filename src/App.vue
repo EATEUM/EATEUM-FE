4352649d377559886bc12e0e74d1827e73235fde
dahyeon <youlove7r@gmail.com>
2025-12-09 22:42:56 +0900

    Chore: Tailwind CSS v4 초기 설정 및 연동

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,17 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
+<script setup></script>
+<!-- tawilwind 확인용 임시! -->
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <h1 class="text-4xl font-bold text-blue-600 underline">Tailwind CSS 연동 성공! 🎉</h1>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <p class="mt-4 text-lg text-gray-700">
+      이 글씨가 보이고 스타일이 적용되었다면 설정이 완벽합니다.
+    </p>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+    <button
+      class="px-6 py-2 mt-8 font-bold text-white transition bg-green-500 rounded-full hover:bg-green-700"
+    >
+      마우스를 올려보세요
+    </button>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
