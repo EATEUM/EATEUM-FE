18472f9b11c1c4dd1c9c64dd2ac4268e9e09f030
dahyeon <youlove7r@gmail.com>
2025-12-20 21:21:13 +0900

    Feat: Nav
    공통 컴포넌트 내브바 UI 구현
    
    Resolves: #19
    Ref: #18

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,30 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import Navbar from './components/layout/Navbar.vue'
 </script>
 
 <template>
-  <div class="flex w-full min-h-screen justify-center bg-[#F0EEE9] font-pretendard">
-    
-      <div class="absolute -left-32 top-10 text-stone-400 text-sm font-bold rotate-90">
-      </div>
-
+  <Navbar />
+  <main class="flex min-h-screen w-full justify-center bg-[#F0EEE9] pt-[65px]">
+    <div>
       <RouterView />
-
-  </div>
+    </div>
+  </main>
 </template>
 
 <style>
 /* Pretendard 웹폰트 적용 */
-@import url("https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css");
+@import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css');
 
 :root {
-  --font-pretendard: "Pretendard Variable", Pretendard, -apple-system, sans-serif;
+  --font-pretendard: 'Pretendard Variable', Pretendard, -apple-system, sans-serif;
 }
 
 body {
   margin: 0;
   font-family: var(--font-pretendard);
   -webkit-font-smoothing: antialiased;
-  background-color: #F0EEE9;
+  background-color: #f0eee9;
 }
 
 .font-pretendard {
